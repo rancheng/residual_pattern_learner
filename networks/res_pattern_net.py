8ab93352fd6b67b3f407bc515635d9dc2405e4de
agiachris <c.agia@yahoo.com>
2020-01-03 16:19:29 -0500

    add res pattern network

---
+++ networks/res_pattern_net.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+class ResidualPatternNetwork(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
