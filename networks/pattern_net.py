a8894633767cd6c702afb18c66d034d4948131d4
agiachris <c.agia@yahoo.com>
2020-01-03 16:24:24 -0500

    add pattern net

---
+++ networks/pattern_net.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+class PatternNetBase(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class PatternMaskNet(PatternNetBase):
+
+    def forward(self):
+        pass
+
+class PatternAnchorNet(PatternNetBase):
+
+    def forward(self):
+        pass
+
