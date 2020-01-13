322d8f5861dc15a9e02bffae2cfcf5ceed755051
agiachris <c.agia@yahoo.com>
2020-01-13 15:26:18 -0500

    residual learner pytorch

--- networks/pattern_net.py
+++ networks/pattern_net.py
@@ -1,87 +1,130 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 
 class ResidualLearner(nn.Module):
 
-    def __init__(self, in_channels, pattern, max_points):
+    def __init__(self, in_channels, default_pattern, k, use_attention=False):
         """Class for learning residual pattern weights via joint segmentation
-        and gaussian blur attention mechanism"""
+        and gaussian blur attention mechanism
         
-        # handles reshape of feature map to weight crops 
-        # and re-weighting default residual pattern
-        self.pattern = ResidualPattern(pattern, max_points)
+        args:
+            in_channels: integer, number of channels in the input image (e.g. RGB + Mask = 4)
+            default_pattern: default residual pattern with shape (m, m)
+            k: after applying weights to default_pattern, take top k scores of scaled pattern
+        """
+        
+        # pattern utility functions helper
+        self.pattern = PatternHelper(default_pattern=default_pattern, max_points=k)
         
         # residual U-Net segmentor
-        self.segmentor = PatternNetBase(in_channels)
+        self.segmentor = UNet(in_channels=in_channels)
         
-        # m must be an odd number (e.g 3x3, 5x5, 11x11, ...)
-        m = pattern.shape[0]
-        self.attention = nn.Conv2d(1, 1, kernel_size=m, stride=1, bias=False, padding=m//2)
+        # attention mechanism (optional)
+        self.use_attention = use_attention
+        if self.use_attention:
+            # m must be an odd number (e.g 3x3, 5x5, 11x11, ...)
+            m = default_pattern.shape[0]
+            self.attention = nn.Conv2d(1, 1, kernel_size=m, stride=1, bias=False, padding=m//2)
 
-    def forward(self, img, points):
-        img = self.segmentor(img)
-        img = self.attention(img)
-        weights = self.pattern.forward(img, points)
-        return x
 
-class ResidualPattern:
-
-    def __init__(self, pattern, max_points):
-        """Computes updated residual patterns given segmentation weights.
+    def forward(self, x, points):
+        """Computes updated residual pattern weights for each point of interest
         args:
-            pattern: np.array with shape (m, m)
-            max_points: number of top scoring points to select 
+            x: image tensor with shape (B, C, H, W), B=1 
+            points: point coordinate tensor with shape (N, 2) 
+
+        returns:
+            weights: residual pattern tensor for each point of interested
+                     with shape (N, m, m)
+        """
+        x = self.segmentor(x)
+
+        if self.use_attention:
+            x = self.attention(x)
+
+        weights = self.pattern.forward(x, points)
+        return weights
+
+class PatternHelper:
+
+    def __init__(self, default_pattern, max_points, threshold=False):
+        """Provides utility functions for computing residual patterns.
+        args:
+            pattern: default tensor pattern with shape (m, m)
+            max_points: number of top scoring elements to select 
                         from the weighted residual pattern
         """
-        self.default_pattern = pattern
+        self.default_pattern = default_pattern
+        self.m = default_pattern.shape[0]
         self.max_points = max_points
+        self.threshold = threshold
+
+        if self.max_points > self.m * self.m:
+            print("Cannot select more points than number of elements in pattern tensor")
+            exit(1)
 
     def compute_pattern(self, weights):
         """Computes the updated residual pattern given weights.
         args:
             weights: np.array with shape (N, m, m)
         """
-        pattern = self.pattern * weights
-        return pattern.round()
+        N = weights.shape[0]
+
+        # element wise product for pattern scaling
+        pattern = self.default_pattern * weights
+
+        # just return thresholded values
+        if self.threshold:
+            return pattern.round()
+        
+        # find top k scoring elements
+        flattened_weights = pattern.reshape(N, self.m*self.m)
+        kth_elem = torch.kthvalue(flattened_weights, self.m * self.m - self.max_points)
+        for i in range(N):
+            weights[i] = torch.gt(weights[i], kth_elem[i])
+        
+        return weights.float()
 
     def img_to_weights(self, weight_map, points):
         """Create (m, m) crops of feature map centered at N points
         args:
-            weight_map: np.array of shape (H, W)
-            points: np,array of shape (N, 2)
+            weight_map: tensor of shape (H, W)
+            points: tensor of shape (N, 2)
         returns:
             weights: tensor of shape (N, m, m)
         """
         
         N = points.shape[0]
-        m = self.pattern.shape[0]
-        half_length = m//2
+        pad = self.m//2
 
-        weight_map_pad = np.pad(weight_map, (m//2, m//2), mode='constant')
-        weights = np.zeros((N, m, m))
+        # pad weight map in case points is at edge of tensor
+        weight_map = F.pad(weight_map, (pad, pad, pad, pad))
+        weights = np.zeros((N, self.m, self.m))
 
         for i in range(N):
             p = points[i]
-            weights[i, ...] = self.crop_from_point(weight_map_pad, p, half_length)
+            weights[i] = self.crop_from_point(weight_map, p, pad)
+
         return weights
 
     @staticmethod
-    def crop_from_point(weight_map, coords, half_length):
-        x, y == coords
-        weights = weight_map[y-half_length:y+half_length, x-half_length:x+half_length]
+    def crop_from_point(weight_map, coords, pad):
+        x, y = coords.long()
+        weights = weight_map[y-pad:y+pad+1, x-pad:x+pad+1]
         return weights
 
     def forward(self, img, points):
-        return self.compute_pattern(self.img_to_weights(img, points))
+        weights = self.img_to_weights(img, points)
+        return self.compute_pattern(weights)
 
 
-class PatternNetBase(nn.Module):
+class UNet(nn.Module):
 
     def __init__(self, in_channels):
-        """Base class for residual pattern module.
+        """Backbone for residual pattern module.
         args:
-            conf: configuration yaml file
+            in_channels: number of input channels for image tensor
         """
         super().__init__()
         
@@ -96,14 +139,8 @@ class PatternNetBase(nn.Module):
         self.outc = OutConv(64, 1)
 
     def forward(self, x):
-        """Default residual pattern index segmented through U-Net 
-        with softmax output over the channels.
-
-        args:
-            x: image tensor (bs, in_channels, h, w)
-        returns:
-            seg: lower resolution segmentation (bs, n_classes, h/4, w/4)
-        """
+        """ Given an image tensor, returns a 1-dimensional feature
+        map [0, 1] with the same spatial resolution as the input"""
         x = self.inc(x)
         x1 = self.down1(x)
         x2 = self.down2(x1)
@@ -121,6 +158,8 @@ class PatternNetBase(nn.Module):
 class OutConv(nn.Module):
     """Output Convolution"""
     def __init__(self, in_c, n_classes):
+        super().__init__()
+
         self.conv = nn.Conv2d(in_c, n_classes, 1, 1)
 
     def forward(self, x):
